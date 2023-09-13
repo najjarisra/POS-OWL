5efe2742853ed0888635262471ed1c6a7e482ed2
mohamed najjari <mnajjari49@gmail.com>
2023-09-13 16:42:14 +0100

    changment css avek hertage payme line

--- static/src/pos/payment_screen/payment_screen.js
+++ static/src/pos/payment_screen/payment_screen.js
@@ -4,15 +4,25 @@ import PaymentScreen from 'point_of_sale.PaymentScreen'
 /**
 * const AExt1 = A => class extends A {}
      * Registry.extend(A, AExt1) */
-const PaymentScreenInherit = (payment_screen) =>class extends payment_screen {
+const PaymentScreenInherit = (payment_screen) => class extends payment_screen {
 
-     setup(){
+    setup() {
         super.setup()
-        console.log('first hiretagehiretagehiretagehiretagehiretagehiretage pos odoo')
+        console.log('first PaymentScreenInherite pos odoo')
 
+
+    }
+    addNewPaymentLine({ detail: paymentMethod }) {
+
+        const PaymentLine1 = super.addNewPaymentLine({ detail: paymentMethod })
+
+        console.log('addNewPayment LineaddNewPaymentLine  addNewPaymentLine')
+
+        console.log(PaymentLine1)
+        return PaymentLine1
 
     }
 
 }
 
-Registries.Component.extend(PaymentScreen,PaymentScreenInherit)
+Registries.Component.extend(PaymentScreen, PaymentScreenInherit)
